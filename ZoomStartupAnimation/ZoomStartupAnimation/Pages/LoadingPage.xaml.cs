6eda6e969b1053de5467f434f0c4eaef18b16319
Marcos Cobeña Morián <marcos@marcoscm.org>
2015-02-01 22:41:26 +0100

    Initial commit 0:-)

---
+++ ZoomStartupAnimation/ZoomStartupAnimation/Pages/LoadingPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using System.Threading.Tasks;
+
+namespace ZoomStartupAnimation
+{
+    public partial class LoadingPage : ContentPage
+    {
+        public LoadingPage()
+        {
+            InitializeComponent();
+
+            Initialize();
+        }
+
+        async void Initialize()
+        {
+            await ExecuteStoryboardAsync();
+            await this.Navigation.PushAsync(new MainPage());
+        }
+
+        async Task ExecuteStoryboardAsync()
+        {
+            await Task.Delay(3000);
+            await LogoImage.ScaleTo(0.75f, length: 1000, easing: Easing.CubicOut);
+            await LogoImage.ScaleTo(25, length: 250, easing: Easing.CubicIn);
+        }
+    }
+}
+
---
+++ ZoomStartupAnimation/ZoomStartupAnimation/Pages/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ZoomStartupAnimation
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            StartAgainButton.Clicked += (sender, args) =>
+                // NOTE: This will generate an infinite navigation, don't do this on real scenarios
+                Navigation.PushAsync(new LoadingPage());
+        }
+    }
+}
+
